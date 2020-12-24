a415622d393e90d53de74abde2ba32b86d7561d1
2654162044 <2654162044@qq.com>
2020-12-24 15:53:59 +0800

    这是第一次创建的测试类

---
+++ src/com/yidu/bean/Test.java
@@ -0,0 +1,11 @@
+package com.yidu.bean;
+
+/**
+ * @description:
+ * @Author shengjianghong
+ * @Date 2020/12/24 0024 下午 3:52
+ * @Version 1.0
+ */
+public class Test {
+    private String name="熊飒";
+}
