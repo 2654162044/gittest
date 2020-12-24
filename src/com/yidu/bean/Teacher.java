543ca7b0f40a2450d062cb0f7db2a91caf41b915
2654162044 <2654162044@qq.com>
2020-12-24 16:04:42 +0800

    这是第er次创建的测试类

---
+++ src/com/yidu/bean/Teacher.java
@@ -0,0 +1,12 @@
+package com.yidu.bean;
+
+/**
+ * @description:
+ * @Author shengjianghong
+ * @Date 2020/12/24 0024 下午 4:01
+ * @Version 1.0
+ */
+public class Teacher {
+    private String name="演员";
+    private String password="123456";
+}
